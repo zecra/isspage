16b3d353d8dd210c93becaa9071457a7f6de2b83
zach <yawq00@gmail.com>
2018-10-06 21:27:39 -0500

    Add files via upload

---
+++ src/script.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+  $("#loading").fadeOut(1000);
+  $(".homepagewrapper").hide().delay(3000).fadeIn(1000);
+});
+$(document).ready(function(){
+  $("#contentwrapper").hide().delay(3000);
+  $("#contentwrapper").slideDown(1000);
+});
